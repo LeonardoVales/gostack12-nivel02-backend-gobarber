b8dfc48e35d93fcca3d7fe7cd78be1a100bca209
Leonardo Vales <leohenrique.vales@gmail.com>
2020-06-29 16:42:37 -0300

    Tabela e model de Usuário. Relacionamento com agendamentos

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,19 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity,
+         Column,
+         PrimaryGeneratedColumn,
+         CreateDateColumn,
+         UpdateDateColumn,
+         ManyToOne,
+         JoinColumn
+    } from 'typeorm';
+
+import User from './User';
+
+/*
+* Um para um (OneToOne) = Um usuário tem um agendamento
+  Um para Muitos (OneToMany) = Um usuário tem muitos agendamentos
+  Muitos para muitos (ManyToMany) = Muitos usuários participam dos mesmos agendamentos
+*/
 
 @Entity('appointments')
 class Appointment {
@@ -6,10 +21,20 @@ class Appointment {
     id: string;
 
     @Column()
-    provider: string;
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({name: 'provider_id'})
+    provider: User;
 
     @Column('timestamp with time zone')
     date: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 
     // constructor({ provider, date}: Omit<Appointment, 'id'>) {
     //     this.id = uuid(),
