90b242b80c07f0a5e1f06914287fefc9b86eb337
Leonardo Vales <leohenrique.vales@gmail.com>
2020-06-29 09:26:18 -0300

    Banco de dados

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,5 +1,5 @@
 import { startOfHour } from 'date-fns';
-
+import { getCustomRepository } from 'typeorm'
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import { endOfDecadeWithOptions } from 'date-fns/fp';
@@ -12,25 +12,28 @@ interface Request {
 
 class CreateAppointmentService {
 
-    private appointmentsRepository: AppointmentsRepository;
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-    }
 
-    public execute({ date, provider }: Request): Appointment {
+    public async execute({ date, provider }: Request): Promise<Appointment> {
+
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+
+
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentInSameDate = appointmentsRepository.findByDate(appointmentDate);
 
         if (findAppointmentInSameDate) {
             throw Error('This appointment is already booked');
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date: appointmentDate
         });
+
+        await appointmentsRepository.save(appointment);
 
         return appointment;
 
