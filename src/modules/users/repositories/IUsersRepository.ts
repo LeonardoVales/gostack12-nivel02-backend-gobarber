307de28f28969d28deeb573c65ceb98c7a6865d9
Leonardo Vales <leohenrique.vales@gmail.com>
2020-08-11 00:08:24 -0300

    Testando criação de usuário

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -4,6 +4,6 @@ import ICreateUserDTO from '../dtos/ICreateUserDTO';
 export default interface IUsersRepository {
     findById(id: string): Promise<User | undefined>;
     findByEmail(email: string): Promise<User | undefined>;
-    create(data: ICreateUserDTO): Promise<User | undefined>;
+    create(data: ICreateUserDTO): Promise<User>;
     save(user: User): Promise<User>;
 }
